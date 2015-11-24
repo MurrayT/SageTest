016ddb124e9f2af5e5664d5f7f3baef436a6c840
Murray Tannock <murraytannock@gmail.com>
2015-11-24 15:32:30 +0000

    Add default paths for produceJSON

--- produceJSON.sh
+++ produceJSON.sh
@@ -7,13 +7,22 @@
 basedir=Student_work-collect
 assigndir=$basedir/$1
 
+if [[ -z "$2"]]; then
+    echo "Assuming default structure"
+    solutionsfile=$1_solutions.sage
+    testcasefile=~/SMCHomeworkGeneration/$1/$1_grading_testcases
+else
+    solutionsfile=$2
+    testcasefile=$3
+fi
+
 if [[ -d $assigndir ]]; then
     for student in $(ls $assigndir); do
-        filename=$assigndir/$student/$2    # this is the file that we're working with
+        filename=$assigndir/$student/$solutionsfile    # this is the file that we're working with
 
         users=`sed -n 's/.*id:\s*\(.*\)/\1/p' $filename`
         if [[ -n "$users" ]]; then
-            sage JSON.sage $filename $3 $users
+            sage JSON.sage $filename $testcasefile $users
         fi
     done
 fi
